371b33fa8f7e35a17741eb806e2a84da4083d26a
FIRST_NAME LAST_NAME <drako1224dragon@gmail.com>
2023-03-19 16:39:51 +0100

    feat: add localStorage save

--- react-task1/src/components/Header.tsx
+++ react-task1/src/components/Header.tsx
@@ -16,11 +16,22 @@ class Header extends Component {
     this.setState({ searchValue: event.target.value });
   };
 
+  componentDidMount() {
+    const searchValue = localStorage.getItem('searchValue');
+    if (searchValue) {
+      this.setState({ searchValue });
+    }
+  }
+
+  componentDidUpdate() {
+    localStorage.setItem('searchValue', this.state.searchValue);
+  }
+
   render() {
     const { cards, searchValue } = this.state;
 
-    const filteredCards = cards.filter(
-      (card: Card) => card.name.toLowerCase().includes(searchValue.toLowerCase()) // card.genre.toLowerCase().includes(searchValue.toLowerCase())
+    const filteredCards = cards.filter((card: Card) =>
+      card.name.toLowerCase().includes(searchValue.toLowerCase())
     );
 
     return (
