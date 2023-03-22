47d5adf59d723cbe976bb9574450b165ee46fae2
FIRST_NAME LAST_NAME <drako1224dragon@gmail.com>
2023-03-22 22:27:39 +0100

    feat: add routes

---
+++ react-task1/src/components/Forms.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Footer from './Footer';
+
+class Forms extends Component {
+  render() {
+    return (
+      <div>
+        Forms to be here
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default Forms;
