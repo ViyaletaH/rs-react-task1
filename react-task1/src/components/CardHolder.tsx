04a96a19c2961492a59b01527ebe80dee9c5dccb
FIRST_NAME LAST_NAME <drako1224dragon@gmail.com>
2023-04-01 21:31:30 +0200

    feat: rewrite CardHolder, Header, Search

--- react-task1/src/components/CardHolder.tsx
+++ react-task1/src/components/CardHolder.tsx
@@ -1,47 +1,42 @@
-import React, { Component } from 'react';
 import { Card } from './data/cards';
 
 export interface Cards {
   cards: Card[];
 }
 
-export class CardHolder extends Component<Cards> {
-  render() {
-    const { cards } = this.props;
-    return (
-      <div className="card-holder">
-        {cards.map((card: Card) => (
-          <div
-            key={card.cardId}
-            data-testid={card.cardId}
-            className="card"
-            onClick={() => window.open(`${card.video}`, '_blank')}
-          >
-            <h2>
-              {card.type}: {card.name}
-            </h2>
-            <img
-              src={`/posters/${card.poster}.PNG`}
-              className="poster"
-              style={{
-                backgroundImage:
-                  card.poster !== '' ? `url( /${card.poster}.png)` : `url(./logo.png)`,
-                backgroundPosition: 'center',
-                backgroundSize: card.poster !== '' ? 'cover' : 'contain',
-                backgroundRepeat: 'no-repeat',
-                backgroundColor: card.poster !== '' ? undefined : '#fff',
-              }}
-              alt="album cover"
-            />
-            <p>Genre: {card.genre}</p>
-            <p>Year: {card.year}</p>
-            <p>Songs: {card.songs}</p>
-            <p>Date of release: {card.date}</p>
-          </div>
-        ))}
-      </div>
-    );
-  }
-}
+const CardHolder = ({ cards }: Cards) => {
+  return (
+    <div className="card-holder">
+      {cards.map((card: Card) => (
+        <div
+          key={card.cardId}
+          data-testid={card.cardId}
+          className="card"
+          onClick={() => window.open(`${card.video}`, '_blank')}
+        >
+          <h2>
+            {card.type}: {card.name}
+          </h2>
+          <img
+            src={`/posters/${card.poster}.PNG`}
+            className="poster"
+            style={{
+              backgroundImage: card.poster !== '' ? `url( /${card.poster}.png)` : `url(./logo.png)`,
+              backgroundPosition: 'center',
+              backgroundSize: card.poster !== '' ? 'cover' : 'contain',
+              backgroundRepeat: 'no-repeat',
+              backgroundColor: card.poster !== '' ? undefined : '#fff',
+            }}
+            alt="album cover"
+          />
+          <p>Genre: {card.genre}</p>
+          <p>Year: {card.year}</p>
+          <p>Songs: {card.songs}</p>
+          <p>Date of release: {card.date}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default CardHolder;
--- react-task1/src/components/Search.tsx
+++ react-task1/src/components/Search.tsx
@@ -1,26 +1,20 @@
-import React, { Component } from 'react';
-
 interface SearchProps {
   onSearchChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export class Search extends Component<SearchProps> {
-  render() {
-    const { onSearchChange } = this.props;
-
-    return (
-      <div className="searcher">
-        <input
-          type="text"
-          placeholder="I'm looking for..."
-          autoFocus
-          className="search"
-          onChange={onSearchChange}
-        />
-        <img src="/search.png" alt="search" className="search-icon" />
-      </div>
-    );
-  }
-}
+const Search = (props: SearchProps) => {
+  return (
+    <div className="searcher">
+      <input
+        type="text"
+        placeholder="I'm looking for..."
+        autoFocus
+        className="search"
+        onChange={props.onSearchChange}
+      />
+      <img src="/search.png" alt="search" className="search-icon" />
+    </div>
+  );
+};
 
 export default Search;
