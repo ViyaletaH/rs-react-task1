60bb12fad8e8849a65178a51b17acc967af07d13
FIRST_NAME LAST_NAME <drako1224dragon@gmail.com>
2023-04-01 19:08:29 +0200

    feat: rewrite About, HeaderBar, Footer, 404

--- react-task1/src/components/AboutUs.tsx
+++ react-task1/src/components/AboutUs.tsx
@@ -1,9 +1,5 @@
-import React, { Component } from 'react';
-
-class AboutUs extends Component {
-  render() {
-    return <h1>About Us</h1>;
-  }
-}
+const AboutUs = () => {
+  return <h1>About Us</h1>;
+};
 
 export default AboutUs;
